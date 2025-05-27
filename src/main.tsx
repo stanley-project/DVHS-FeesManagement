3c5670f48ac7f9515108568ba5a66ffd75143a6a
stanley-project <85718695+stanley-project@users.noreply.github.com>
2025-05-27 14:25:28 +0530

    Remove duplicate VillageProvider import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { VillageProvider } from './contexts/VillageContext';
-import { VillageProvider } from './contexts/VillageContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -12,9 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <AuthProvider>
         <VillageProvider>
-        <VillageProvider>
           <App />
-        </VillageProvider>
         </VillageProvider>
       </AuthProvider>
     </BrowserRouter>
