96c52c34c4ff2ca2fe62182cd5d1f39e0326b9ec
stanley-project <85718695+stanley-project@users.noreply.github.com>
2025-05-29 15:52:54 +0530

    Updated AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -197,7 +197,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     } finally {
       if (isMounted.current) {
         setAuthLoading(false);
-        setLoginCode(null);
+//        setLoginCode(null);
       }
     }
   };
