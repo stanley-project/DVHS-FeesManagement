52de841f57a4e3c7f2dfb1e8581ff167137946f8
stanley-project <85718695+stanley-project@users.noreply.github.com>
2025-06-25 15:10:53 +0530

    Remove unused VillageContext and provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
-import { VillageProvider } from './contexts/VillageContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <AuthProvider>
-        <VillageProvider>
-          <App />
-        </VillageProvider>
+        <App />
       </AuthProvider>
     </Router>
   </React.StrictMode>
